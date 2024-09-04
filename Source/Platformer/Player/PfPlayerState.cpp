93dbfa8abb1efbe0b3e8cfa949dbde064117b5b4
hyoslog <98869559+hyoslog@users.noreply.github.com>
2024-09-04 23:10:05 +0900

    GAS 연동
    - PfPlayerState 빈 클래스 추가

---
+++ Source/Platformer/Player/PfPlayerState.cpp
@@ -0,0 +1,18 @@
+// Copyright Kang Hyo Jin. All Rights Reserved.
+
+// Default
+#include "PfPlayerState.h"
+
+// C++ Standard Library
+
+// Third-party Library
+
+// Unreal Engine
+
+// Project-specific
+
+// Local
+
+APfPlayerState::APfPlayerState()
+{
+}
---
+++ Source/Platformer/Player/PfPlayerState.h
@@ -0,0 +1,28 @@
+// Copyright Kang Hyo Jin. All Rights Reserved.
+
+#pragma once
+
+// Default
+
+// C++ Standard Library
+
+// Third-party Library
+
+// Unreal Engine
+#include "GameFramework/PlayerState.h"
+
+// Project-specific
+
+// Local
+
+// Last
+#include "PfPlayerState.generated.h"
+
+UCLASS()
+class PLATFORMER_API APfPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	APfPlayerState();
+};
